3c90fdad5a8431163cac37adb77426250a33d364
Saksham <sakshamsatnalika34@gmail.com>
2025-10-22 12:42:02 +0530

    backend link api fixed

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://cinevibe-server-uhq8.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
+  withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
   },
